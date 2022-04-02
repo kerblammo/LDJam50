ce260967b99b5c58114a88153e8ccbe605a1334b
Peter Adam <padamCKB@hotmail.com>
2022-04-02 19:23:26 -0300

    Enemy spawning now has random locations offscreen.

--- LDJam50/Assets/Scripts/Spawner.cs
+++ LDJam50/Assets/Scripts/Spawner.cs
@@ -7,7 +7,7 @@ public class Spawner : MonoBehaviour
     [SerializeField] Enemy enemy;
     [SerializeField] int spawnCount;
     [SerializeField] int waveCount = 0;
-
+    [SerializeField] List<Collider2D> spawnZones;
     public void NextWave()
     {
         waveCount++;
@@ -17,11 +17,25 @@ public class Spawner : MonoBehaviour
         List<Enemy> enemies = new List<Enemy>();
         for (int i = 0; i < spawnCount; i++)
         {
-            Enemy karen = Instantiate(enemy, transform.position, Quaternion.identity);
+            Vector3 spawnPoint = CalculateRandomSpawnPosition();
+            Enemy karen = Instantiate(enemy, spawnPoint, Quaternion.identity);
             enemies.Add(karen);
         }
 
         return enemies;
+    }
+
+    Vector3 CalculateRandomSpawnPosition()
+    {
+        int i = Random.Range(0, spawnZones.Count);
+        Debug.Log(i);
+        Collider2D spawnZone = spawnZones[i];
+        Bounds bounds = spawnZone.bounds;
+        return new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                0
+            );
     }
 
 }
