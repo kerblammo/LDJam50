009a890131ed6eb0b7e3f7f842c26bce31063e8e
Peter Adam <padamCKB@hotmail.com>
2022-04-04 17:49:00 -0300

    Mute button now updates to show if volume is currently muted.

--- LDJam50/Assets/Scripts/SoundController.cs
+++ LDJam50/Assets/Scripts/SoundController.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class SoundController : MonoBehaviour
 {
     [SerializeField] AudioMixer sfx;
+    [SerializeField] TextMeshProUGUI buttonLabel;
     private void Awake()
     {
         int numSoundControllers = FindObjectsOfType<SoundController>().Length;
@@ -25,13 +27,23 @@ public class SoundController : MonoBehaviour
 
         if (volume <= -80f)
         {
+            UpdateButtonLabel(true);
             volume = 0;
         }
         else
         {
+            UpdateButtonLabel(false);
             volume = -80f;
         }
 
         sfx.SetFloat("Volume", volume);
+    }
+
+    void UpdateButtonLabel(bool soundIsOn)
+    {
+        // if sound is on, the label should give the player the option to turn it off
+        string state = soundIsOn ? "off" : "on";
+        string label = $"Turn {state} the store radio!";
+        buttonLabel.text = label;
     }
 }
